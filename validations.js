e721f39d5803d773e61ce1648bb9061a0389bfb1
Anastasia <nastyakopylova1912.02@gmail.com>
2023-04-18 20:35:19 +0400

    validation: register, login, postCreate

--- validations.js
+++ validations.js
@@ -1,12 +1,12 @@
 import { body } from 'express-validator';
 
-//создание валидации авторизации
+// //создание валидации авторизации
 export const loginValidation = [
     body('email','Неверный формат почты').isEmail(),
     body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5}),
 ];
 
-//создание валидации регистрации
+// //создание валидации регистрации
 export const registerValidation = [
     body('email','Неверный формат почты').isEmail(),
     body('password', 'Пароль должен быть минимум 5 символов').isLength({ min: 5}),
@@ -14,11 +14,10 @@ export const registerValidation = [
     body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
 ];
 
-//создание валидации статьи
+// //создание валидации статьи
 export const postCreateValidation = [
     body('title','Введите заголовок статьи').isLength({min: 3}).isString(),
     body('text', 'Введите текст статьи').isLength({ min: 10}).isString(),
     body('tags', 'Неверный формат тэгов (укажите массив)').optional().isString(),
     body('imageUrl', 'Неверная ссылка на изображение').optional().isString(), // optional() - у статьи может быть картинка, а может и не быть
 ];
-//
