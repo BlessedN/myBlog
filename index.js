9d72e33656f707d33120719c00704316ecd589db
Anastasia <nastyakopylova1912.02@gmail.com>
2023-04-29 00:39:48 +0400

    Ready route login/register/getMe and getAll/getOne/create/remove/update

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import mongoose from "mongoose";
-import { loginValidation, registerValidation } from "./validations.js";
+import { loginValidation, registerValidation, postCreateValidation } from "./validations.js";
 import checkAuth from "./utils/checkAuth.js";
 import { validationResult } from "express-validator";
 import { register, login, getMe } from './controllers/UserController.js';
-import { UserController } from './controllers/index.js';
+// import { UserController } from './controllers/index.js';
+// import { PostController } from './controllers/index.js';
+import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
 
 mongoose
   .connect("mongodb+srv://mongo:mongo123@cluster0.ybbmxdp.mongodb.net/infablog?retryWrites=true&w=majority")
@@ -13,11 +16,17 @@ mongoose
 
 const app = express();
 
-app.use(express.json()); // из-за этого была ошибка
+app.use(express.json());                          // из-за этого была ошибка
 
 app.post('/auth/login',loginValidation, UserController.login);
 app.post('/auth/register',registerValidation, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts',checkAuth, postCreateValidation, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', PostController.update);
 
 
 
--- validations.js
+++ validations.js
@@ -15,9 +15,9 @@ export const registerValidation = [
 ];
 
 // //создание валидации статьи
-// export const postCreateValidation = [
-//     body('title','Введите заголовок статьи').isLength({min: 3}).isString(),
-//     body('text', 'Введите текст статьи').isLength({ min: 10}).isString(),
-//     body('tags', 'Неверный формат тэгов (укажите массив)').optional().isString(),
-//     body('imageUrl', 'Неверная ссылка на изображение').optional().isString(), // optional() - у статьи может быть картинка, а может и не быть
-// ];
+export const postCreateValidation = [
+    body('title','Введите заголовок статьи').isLength({min: 3}).isString(),
+    body('text', 'Введите текст статьи').isLength({ min: 10}).isString(),
+    body('tags', 'Неверный формат тэгов (укажите массив)').optional().isString(),
+    body('imageUrl', 'Неверная ссылка на изображение').optional().isString(), // optional() - у статьи может быть картинка, а может и не быть
+];
